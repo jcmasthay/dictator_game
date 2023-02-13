a8c2c2ad87430f4781375668c99ef4dc74351810
jcmasthay <changlabneuro>
2023-02-13 12:46:14 -0500

    add debug script

---
+++ script/dg_run_debug_task.m
@@ -0,0 +1,43 @@
+trial_set = dg.task.TrialSet();
+% 
+% trial0 = dg.task.TrialDescriptor();
+% trial0.TrialType = 'train-choice';
+% trial0.Outcomes = { 'self', 'bottle' };
+% trial0.DelayToDecision = 0.6;
+% trial0.DelayToReward = 0.8;
+% trial0.RewardChannels = [1, 2];
+
+trial0 = dg.task.TrialDescriptor();
+trial0.TrialType = 'train-cued';
+trial0.Outcomes = { 'self' };
+trial0.DelayToDecision = 0.0;
+trial0.DelayToReward = 2;
+trial0.RewardChannels = 2;
+trial0.ChoiceTargetEccentricities = { 'center' };
+
+% trial0 = dg.task.TrialDescriptor();
+% trial0.TrialType = 'train-choice';
+% trial0.Outcomes = { 'self' };
+% trial0.DelayToDecision = 0.0;
+% trial0.DelayToReward = 2;
+% trial0.RewardChannels = 2;
+% trial0.ChoiceTargetEccentricities = { 'top-right' };
+
+trial_set.Trials = [trial0, trial0];
+
+conf = dg.config.create;
+
+conf.windows.main.index = 0;
+conf.windows.main.rect = [0, 0, 800, 800];
+
+conf.serial.disabled = false;
+data = dg.task.run( conf, trial_set );
+
+%%
+
+%{
+
+define number of trials in a block
+decide stimuli for cues and choice targets, in all trial types
+
+%}
